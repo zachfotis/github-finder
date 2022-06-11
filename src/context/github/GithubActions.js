016305c5b16926c34df02eb359db7877626ebecf
Fotios Zachopoulos <65657774+zachfotis@users.noreply.github.com>
2022-06-12 01:01:20 +0300

    remove token

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const github = axios.create({
   baseURL: process.env.REACT_APP_GITHUB_URL,
-  headers: { Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}` },
+  // headers: { Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}` },
 });
 
 // Get search results
